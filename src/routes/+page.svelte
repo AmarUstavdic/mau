52793abed1a7e9c9cb8125fa6e64a8a699a3a2f1
amar <ustavdica.private@gmail.com>
2024-05-11 19:00:53 +0200

    connect/disconnect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	interface ConnectInfo {
+		chainId: string;
+	}
+
+	const getMetaMaskPresent = () => window.ethereum.isMetaMask;
+
+	let provider;
+
+	const handleConnect = async () => {
+		if (window.ethereum) {
+			const res = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+			console.log(res);
+		}
+	};
+
+	const handleDisconnect = async () => {
+		if (window.ethereum) {
+			await window.ethereum.request({
+				method: 'wallet_revokePermissions',
+				params: [
+					{
+						eth_accounts: {}
+					}
+				]
+			});
+		}
+	};
+
+	onMount(() => {
+		provider = window.ethereum;
+
+		console.log(getMetaMaskPresent());
+	});
+</script>
+
+<button on:click={handleConnect}>Connect</button>
+<button on:click={handleDisconnect}>Disconnect</button>
