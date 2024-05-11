0bc142ca00221264185167db9f054771923a0078
UNovak <88665587+UNovak@users.noreply.github.com>
2024-05-11 22:43:40 +0200

    Implement supabase client initialization

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,12 @@
+import { supabase } from "$lib/supabase";
+
+export async function load() {
+  const { data } = await supabase.from("countries").select();
+  return {
+    countries: data ?? [],
+  };
+}
+
+// make requests to supabase here
+          
+
